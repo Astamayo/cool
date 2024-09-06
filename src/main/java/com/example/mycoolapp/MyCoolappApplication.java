4906c194acd7f7ca9c58b17c9b9256e9d37f28fe
Astamayo <antoniosilverio21@gmail.com>
2024-09-06 05:24:30 -0400

    Initial commit

---
+++ src/main/java/com/example/mycoolapp/MyCoolappApplication.java
@@ -0,0 +1,14 @@
+package com.example.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MyCoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCoolappApplication.class, args);
+	}
+
+}
